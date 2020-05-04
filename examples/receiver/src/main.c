65f67d3823e7bfaf83bd7ad14ac3763e3efb55f8
ChristianReese <christian.g.reese96@gmail.com>
2020-05-04 13:22:31 -0500

    Input-based receiver example (untested!)

--- examples/receiver/src/main.c
+++ examples/receiver/src/main.c
@@ -56,6 +56,7 @@ typedef struct SourceData
   char name[SACN_SOURCE_NAME_MAX_LEN];
   uint8_t priority;
   int num_updates;
+  uint32_t update_start_time_ms;
   uint8_t last_update[NUM_SLOTS_DISPLAYED];
 } SourceData;
 
@@ -92,11 +93,57 @@ static SourceData* find_source_hole(ListeningUniverse* listener)
     if (!source->valid)
       return source;
   }
+
   return NULL;
 }
 
+static void invalidate_listener_handles()
+{
+  for (ListeningUniverse* listener = listeners; listener < listeners + MAX_LISTENERS; ++listener)
+  {
+    listener->receiver_handle = SACN_RECEIVER_INVALID;
+  }
+}
+
+static ListeningUniverse* find_listener_hole()
+{
+  for (ListeningUniverse* listener = listeners; listener < listeners + MAX_LISTENERS; ++listener)
+  {
+    if (listener->receiver_handle == SACN_RECEIVER_INVALID)
+    {
+      return listener;
+    }
+  }
+
+  return NULL;
+}
+
+static ListeningUniverse* find_listener_on_universe(int universe)
+{
+  for (ListeningUniverse* listener = listeners; listener < listeners + MAX_LISTENERS; ++listener)
+  {
+    if (listener->universe == universe)
+    {
+      return listener;
+    }
+  }
+
+  return NULL;
+}
+
+static void console_print_help()
+{
+  printf("Each input is listed followed by the action:\n");
+  printf("h : Print help.\n");
+  printf("p : Print updates for all universe listeners.\n");
+  printf("a : Add a new universe listener.\n");
+  printf("r : Remove a universe listener.\n");
+  printf("c : Change a listener's universe.\n");
+  printf("ctrl-c : Exit.\n");
+}
+
 /* Print a status update for each listening universe about the status of its sources. */
-void print_universe_updates(int interval_ms)
+static void console_print_universe_updates()
 {
   if (etcpal_mutex_lock(&mutex))
   {
@@ -113,6 +160,7 @@ void print_universe_updates(int interval_ms)
           {
             char cid_str[ETCPAL_UUID_STRING_BYTES];
             etcpal_uuid_to_string(&source->cid, cid_str);
+            int interval_ms = etcpal_getms() - source->update_start_time_ms;
             int update_rate = source->num_updates * 1000 / interval_ms;
             printf("  Source %s\tPriority: %u\tUpdates per second: %d\tLast update: ", cid_str, source->priority,
                    update_rate);
@@ -122,6 +170,7 @@ void print_universe_updates(int interval_ms)
             }
             printf("Name: '%s'\n", source->name);
             source->num_updates = 0;
+            source->update_start_time_ms = etcpal_getms();
           }
         }
       }
@@ -129,6 +178,77 @@ void print_universe_updates(int interval_ms)
     printf("-------------------------------------------------------------------------------------------------------\n");
     etcpal_mutex_unlock(&mutex);
   }
+}
+
+static etcpal_error_t console_add_listening_universe(SacnReceiverCallbacks* callbacks)
+{
+  etcpal_error_t result = kEtcPalErrOk;
+  ListeningUniverse* new_listener = find_listener_hole();
+
+  if (new_listener)
+  {
+    int universe = 0;
+    printf("Enter the universe number:\n");
+    scanf("%d", &universe);
+    result = create_listener(new_listener, universe, callbacks);
+  }
+  else
+  {
+    printf("Maximum number of listeners has been reached. Please remove a listener first before adding a new one.\n");
+    result = kEtcPalErrNoMem;
+  }
+
+  printf("Result: %s\n", etcpal_strerror(result));
+
+  return result;
+}
+
+static etcpal_error_t console_remove_listening_universe()
+{
+  etcpal_error_t result = kEtcPalErrOk;
+
+  int universe = 0;
+  printf("Enter the universe number:\n");
+  scanf("%d", &universe);
+
+  ListeningUniverse* listener = find_listener_on_universe(universe);
+  if (listener)
+  {
+    result = destroy_listener(listener);
+    printf("Result: %s\n", etcpal_strerror(result));
+  }
+  else
+  {
+    printf("There are no listeners currently listening to universe %d.\n", universe);
+  }
+
+  return result;
+}
+
+static etcpal_error_t console_change_listening_universe(SacnReceiverCallbacks* callbacks)
+{
+  etcpal_error_t result = kEtcPalErrOk;
+
+  int current_universe = 0;
+  printf("Enter the current universe number:\n");
+  scanf("%d", &current_universe);
+
+  ListeningUniverse* listener = find_listener_on_universe(current_universe);
+  if (listener)
+  {
+    int new_universe = 0;
+    printf("Enter the new universe number:\n");
+    scanf("%d", &new_universe);
+
+    result = update_listener_universe(listener, new_universe, callbacks);
+    printf("Result: %s\n", etcpal_strerror(result));
+  }
+  else
+  {
+    printf("There are no listeners currently listening to universe %d.\n", current_universe);
+  }
+
+  return result;
 }
 
 static etcpal_error_t update_listener_universe(ListeningUniverse* listener, uint16_t new_universe,
@@ -241,6 +361,7 @@ static void handle_universe_data(sacn_receiver_t handle, const EtcPalSockAddr* f
         strcpy(source->name, header->source_name);
         source->priority = header->priority;
         source->num_updates = 1;
+        source->update_start_time_ms = etcpal_getms();
         memcpy(source->last_update, pdata, NUM_SLOTS_DISPLAYED);
         source->valid = true;
         ++listener->num_sources;
@@ -369,24 +490,52 @@ int main(void)
     return 1;
   }
 
+  // Initialize the listener array by making each handle invalid since there are no listeners to start.
+  invalidate_listener_handles();
+
   // Handle Ctrl+C gracefully and shut down in compatible consoles
   install_keyboard_interrupt_handler(handle_keyboard_interrupt);
 
   // Handle user input until we are told to stop
+  bool print_prompt = true;
   while (keep_running)
   {
+    if (print_prompt)
+    {
+      printf("Enter input (enter h for help):\n");
+    }
+    else
+    {
+      print_prompt = true;  // Print it next time by default.
+    }
+
+    etcpal_error_t result = kEtcPalErrOk;
     int ch = getchar();
     switch (ch)
     {
+      case 'h':
+        console_print_help();
+        break;
+      case 'p':
+        console_print_universe_updates();
+        break;
       case 'a':
-        printf("You pressed a.\n");
+        result = console_add_listening_universe(&kSacnCallbacks);
+        break;
+      case 'r':
+        result = console_remove_listening_universe();
+        break;
+      case 'c':
+        result = console_change_listening_universe(&kSacnCallbacks);
         break;
       case '\n':
+        print_prompt = false;  // Otherwise the prompt is printed twice.
         break;
       case EOF:  // Ctrl-C
         keep_running = false;
+        break;
       default:
-        printf("You pressed %d.\n", ch);
+        printf("Invalid input.\n");
     }
   }
 
