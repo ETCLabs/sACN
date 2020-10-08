9f5cba18f33e0333361c4613b551e26c8409cf0e
ChristianReese <christian.g.reese96@gmail.com>
2020-10-08 10:20:34 -0500

    Block source_pap_lost during the sampling period

--- src/sacn/receiver.c
+++ src/sacn/receiver.c
@@ -1152,14 +1152,18 @@ void process_null_start_code(const SacnReceiver* receiver, SacnTrackedSource* sr
     case kRecvStateHaveDmxAndPap:
       if (etcpal_timer_is_expired(&src->pap_timer))
       {
-        // Source stopped sending PAP but is still sending DMX. In this case, also notify the
-        // source_pap_lost callback.
-        source_pap_lost->callback = receiver->callbacks.source_pap_lost;
-        source_pap_lost->source.cid = src->cid;
-        ETCPAL_MSVC_NO_DEP_WRN strcpy(source_pap_lost->source.name, src->name);
-        source_pap_lost->context = receiver->callback_context;
-        source_pap_lost->handle = receiver->keys.handle;
-        source_pap_lost->universe = receiver->keys.universe;
+        // Source stopped sending PAP but is still sending DMX.
+        if (*notify)
+        {
+          // In this case, also notify the source_pap_lost callback.
+          source_pap_lost->callback = receiver->callbacks.source_pap_lost;
+          source_pap_lost->source.cid = src->cid;
+          ETCPAL_MSVC_NO_DEP_WRN strcpy(source_pap_lost->source.name, src->name);
+          source_pap_lost->context = receiver->callback_context;
+          source_pap_lost->handle = receiver->keys.handle;
+          source_pap_lost->universe = receiver->keys.universe;
+        }
+
         src->recv_state = kRecvStateHaveDmxOnly;
       }
       break;
