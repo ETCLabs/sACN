1cde02837575b7d965c00bc794997f5e1c3957c6
ChristianReese <Christian.Reese@etcconnect.com>
2020-12-16 13:48:51 -0600

    DMX Merger New Update API (#19)
    * Updated C API (still need to update C++ side and using_dmx_merger.md)
    
    * Updated C++ API
    
    * Documentation corrections
    
    * Update using_dmx_merger.md
    
    * values -> levels

--- include/sacn/cpp/dmx_merger.h
+++ include/sacn/cpp/dmx_merger.h
@@ -46,11 +46,11 @@ namespace sacn
  *
  * When asked to calculate the merge, the merger will evaluate the current source
  * buffers and update two result buffers:
- *  - 512 bytes for the merged data values (i.e. "winning level").  These are calculated by using
+ *  - 512 bytes for the merged data levels (i.e. "winning level").  These are calculated by using
  *     a Highest-Level-Takes-Precedence(HTP) algorithm for all sources that share the highest
  *     per-address priority.
  *  - 512 source identifiers (i.e. "winning source") to indicate which source was considered the
- *     source of the merged data value, or that no source currently owns this address.
+ *     source of the merged data level, or that no source currently owns this address.
  *
  * See @ref using_dmx_merger for a detailed description of how to use this API.
  */
@@ -86,7 +86,7 @@ public:
     uint8_t* per_address_priorities{nullptr};
 
     /** Buffer of #DMX_ADDRESS_COUNT source IDs that indicate the current winner of the merge for that slot, or
-        #SACN_DMX_MERGER_SOURCE_INVALID to indicate that no source is providing values for that slot. This is used if
+        #SACN_DMX_MERGER_SOURCE_INVALID to indicate that there is no winner for that slot. This is used if
         you need to know the source of each slot. If you only need to know whether or not a slot is sourced, set this to
         NULL and use per_address_priorities (which has half the memory footprint) to check if the slot has a priority of
         0 (not sourced).
@@ -120,10 +120,11 @@ public:
   etcpal::Expected<sacn_source_id_t> AddSource();
   etcpal::Error RemoveSource(sacn_source_id_t source);
   const SacnDmxMergerSource* GetSourceInfo(sacn_source_id_t source) const;
-  etcpal::Error UpdateSourceData(sacn_source_id_t source, uint8_t priority, const uint8_t* new_values,
-                                 size_t new_values_count, const uint8_t* address_priorities = nullptr,
-                                 size_t address_priorities_count = 0);
-  etcpal::Error StopSourcePerAddressPriority(sacn_source_id_t source);
+
+  etcpal::Error UpdateLevels(sacn_source_id_t source, const uint8_t* new_levels, size_t new_levels_count);
+  etcpal::Error UpdatePaps(sacn_source_id_t source, const uint8_t* paps, size_t paps_count);
+  etcpal::Error UpdateUniversePriority(sacn_source_id_t source, uint8_t universe_priority);
+  etcpal::Error RemovePaps(sacn_source_id_t source);
 
   constexpr Handle handle() const;
 
@@ -242,35 +243,77 @@ inline const SacnDmxMergerSource* DmxMerger::GetSourceInfo(sacn_source_id_t sour
 }
 
 /**
- * @brief Updates the source data and recalculate outputs.
+ * @brief Updates a source's levels and recalculates outputs.
+ *
+ * This function updates the levels of the specified source, and then triggers the recalculation of each slot. For each
+ * slot, the source will only be included in the merge if it has a level and a priority at that slot.
  *
  * @param[in] source The id of the source to modify.
- * @param[in] priority The universe-level priority of the source.
- * @param[in] new_values The new DMX values to be copied in. This must be nullptr if the source is not updating DMX
- * data.
- * @param[in] new_values_count The length of new_values. Must be 0 if the source is not updating DMX data.
- * @param[in] address_priorities The per-address priority values to be copied in.  This must be nullptr if the source is
- * not updating per-address priority data.
- * @param[in] address_priorities_count The length of address_priorities.  Must be 0 if the source is not updating
- * per-address priority data.
+ * @param[in] new_levels The new DMX levels to be copied in.
+ * @param[in] new_levels_count The length of new_levels.
  * @return #kEtcPalErrOk: Source updated and merge completed.
  * @return #kEtcPalErrInvalid: Invalid parameter provided.
  * @return #kEtcPalErrNotInit: Module not initialized.
  * @return #kEtcPalErrNotFound: Handle does not correspond to a valid source or merger.
  * @return #kEtcPalErrSys: An internal library or system call error occurred.
  */
-inline etcpal::Error DmxMerger::UpdateSourceData(sacn_source_id_t source, uint8_t priority, const uint8_t* new_values,
-                                                 size_t new_values_count, const uint8_t* address_priorities,
-                                                 size_t address_priorities_count)
+inline etcpal::Error DmxMerger::UpdateLevels(sacn_source_id_t source, const uint8_t* new_levels,
+                                             size_t new_levels_count)
 {
-  return sacn_dmx_merger_update_source_data(handle_, source, priority, new_values, new_values_count, address_priorities,
-                                            address_priorities_count);
+  return sacn_dmx_merger_update_levels(handle_, source, new_levels, new_levels_count);
 }
 
 /**
- * @brief Removes the per-address data from the source and recalculate outputs.
+ * @brief Updates a source's per-address priorities (PAPs) and recalculates outputs.
  *
- * Per-address priority data can time out in sACN just like values.
+ * This function updates the per-address priorities (PAPs) of the specified source, and then triggers the recalculation
+ * of each slot. For each slot, the source will only be included in the merge if it has a level and a priority at that
+ * slot.
+ *
+ * If PAPs are not specified for all slots, then the remaining slots will default to a PAP of 0. To remove PAPs for this
+ * source and revert to the universe priority, call DmxMerger::RemovePaps.
+ *
+ * @param[in] source The id of the source to modify.
+ * @param[in] paps The per-address priorities to be copied in.
+ * @param[in] paps_count The length of paps.
+ * @return #kEtcPalErrOk: Source updated and merge completed.
+ * @return #kEtcPalErrInvalid: Invalid parameter provided.
+ * @return #kEtcPalErrNotInit: Module not initialized.
+ * @return #kEtcPalErrNotFound: Handle does not correspond to a valid source or merger.
+ * @return #kEtcPalErrSys: An internal library or system call error occurred.
+ */
+inline etcpal::Error DmxMerger::UpdatePaps(sacn_source_id_t source, const uint8_t* paps, size_t paps_count)
+{
+  return sacn_dmx_merger_update_paps(handle_, source, paps, paps_count);
+}
+
+/**
+ * @brief Updates a source's universe priority and recalculates outputs.
+ *
+ * This function updates the universe priority of the specified source, and then triggers the recalculation of each
+ * slot. For each slot, the source will only be included in the merge if it has a level and a priority at that slot.
+ *
+ * If per-address priorities (PAPs) were previously specified for this source with DmxMerger::UpdatePaps, then the
+ * universe priority can have no effect on the merge results until the application calls DmxMerger::RemovePaps, at which
+ * point the priorities of each slot will revert to the universe priority passed in here.
+ *
+ * @param[in] source The id of the source to modify.
+ * @param[in] universe_priority The universe-level priority of the source.
+ * @return #kEtcPalErrOk: Source updated and merge completed.
+ * @return #kEtcPalErrInvalid: Invalid parameter provided.
+ * @return #kEtcPalErrNotInit: Module not initialized.
+ * @return #kEtcPalErrNotFound: Handle does not correspond to a valid source or merger.
+ * @return #kEtcPalErrSys: An internal library or system call error occurred.
+ */
+inline etcpal::Error DmxMerger::UpdateUniversePriority(sacn_source_id_t source, uint8_t universe_priority)
+{
+  return sacn_dmx_merger_update_universe_priority(handle_, source, universe_priority);
+}
+
+/**
+ * @brief Removes the per-address priority (PAP) data from the source and recalculate outputs.
+ *
+ * Per-address priority data can time out in sACN just like levels.
  * This is a convenience function to immediately turn off the per-address priority data for a source and recalculate the
  * outputs.
  *
@@ -280,7 +323,7 @@ inline etcpal::Error DmxMerger::UpdateSourceData(sacn_source_id_t source, uint8_
  * @return #kEtcPalErrNotInit: Module not initialized.
  * @return #kEtcPalErrSys: An internal library or system call error occurred.
  */
-inline etcpal::Error DmxMerger::StopSourcePerAddressPriority(sacn_source_id_t source)
+inline etcpal::Error DmxMerger::RemovePaps(sacn_source_id_t source)
 {
   return sacn_dmx_merger_stop_source_per_address_priority(handle_, source);
 }
