7b324d963d320e02805ecd54263cd06a1803425b
ETCLabs CI <noreply.etclabs@etcconnect.com>
2020-12-29 16:17:42 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20201229.1
    
    Commit: 658bcec58998f33f3dfb9a468ce98558e4ef09f1

---
+++ docs/stage/24/navtreedata.js
@@ -0,0 +1,47 @@
+var NAVTREE =
+[
+  [ "sACN", "index.html", [
+    [ "Streaming ACN", "index.html", null ],
+    [ "Additional Documentation", "additional_documentation.html", "additional_documentation" ],
+    [ "Getting Started with sACN", "getting_started.html", "getting_started" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"additional_documentation.html",
+"group__sacn__merge__receiver.html#ga41ff5370e2f5b4df7b2d6d719c278a28",
+"merge__receiver_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ docs/stage/24/search/groups_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['global_20options',['Global Options',['../group__sacnopts__global.html',1,'']]]
+];
