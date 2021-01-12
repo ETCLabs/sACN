74f69d44f76ae857a179113e1edc09f7ddb5ca1a
ChristianReese <christian.g.reese96@gmail.com>
2021-01-12 12:35:54 -0600

    UNIVERSE_ID_VALID fix

--- src/sacn/private/common.h
+++ src/sacn/private/common.h
@@ -118,7 +118,7 @@ typedef unsigned int sacn_thread_id_t;
 #define SACN_CAN_LOG(pri) false
 #endif
 
-#define UNIVERSE_ID_VALID(universe_id) ((universe_id != 0) && (universe_id <= 64000))
+#define UNIVERSE_ID_VALID(universe_id) ((universe_id != 0) && (universe_id < 64000))
 
 /******************************************************************************
  * Types used by the source loss module
