99d0743a7c6d12d380d2e3123a5fb3d1fa5af0eb
ETCLabs CI <noreply.etclabs@etcconnect.com>
2021-01-22 17:00:54 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20210122.1
    
    Commit: 46ed87f2019d31dede2805996f518223c551d10c

---
+++ docs/stage/32/group__sacn__dmx__merger.js
@@ -0,0 +1,34 @@
+var group__sacn__dmx__merger =
+[
+    [ "SacnDmxMergerConfig", "struct_sacn_dmx_merger_config.html", [
+      [ "per_address_priorities", "struct_sacn_dmx_merger_config.html#a6b8790b29687764423319cc2b8f24c30", null ],
+      [ "slot_owners", "struct_sacn_dmx_merger_config.html#a52db3df3a97298a9d96b7559fded9128", null ],
+      [ "slots", "struct_sacn_dmx_merger_config.html#a08bc75bcabb0a92022eac3a623f32ff6", null ],
+      [ "source_count_max", "struct_sacn_dmx_merger_config.html#aa56795d5b57bff4910ce11baac4003ba", null ]
+    ] ],
+    [ "SacnDmxMergerSource", "struct_sacn_dmx_merger_source.html", [
+      [ "address_priority", "struct_sacn_dmx_merger_source.html#a8e2ad703e06552317b7f47dd0e0ebbea", null ],
+      [ "address_priority_valid", "struct_sacn_dmx_merger_source.html#ab2fb729fbb80e2b480e4466e8cbf9b57", null ],
+      [ "id", "struct_sacn_dmx_merger_source.html#a6f908810ffc25f89449b60177428980b", null ],
+      [ "levels", "struct_sacn_dmx_merger_source.html#a82f491441fd8ea7e0b36c867797a9ba5", null ],
+      [ "universe_priority", "struct_sacn_dmx_merger_source.html#aefc54b6cd291e480d6cb7c9b5b98a3f4", null ],
+      [ "valid_level_count", "struct_sacn_dmx_merger_source.html#a3695ed7e336fa1e6cf75ee3ccbc4937d", null ]
+    ] ],
+    [ "SACN_DMX_MERGER_CONFIG_INIT", "group__sacn__dmx__merger.html#ga7431ab2baf3af7e0d1999355c6bcc9d7", null ],
+    [ "SACN_DMX_MERGER_INVALID", "group__sacn__dmx__merger.html#ga4578a59809c13ece174e8dcf59fce26d", null ],
+    [ "SACN_DMX_MERGER_SOURCE_INVALID", "group__sacn__dmx__merger.html#ga1ca9e023e2091c5a8a4e5e2ba426f055", null ],
+    [ "SACN_DMX_MERGER_SOURCE_IS_VALID", "group__sacn__dmx__merger.html#ga7b7762d0e6afca73eb8c31fa55354439", null ],
+    [ "sacn_dmx_merger_t", "group__sacn__dmx__merger.html#gabee79fb378d5942866adc898cb7da38b", null ],
+    [ "sacn_source_id_t", "group__sacn__dmx__merger.html#gab8d8887ff8ef5fbfa4a39a9db5230a49", null ],
+    [ "SacnDmxMergerConfig", "group__sacn__dmx__merger.html#ga40f96dd6f9ef2598f03b2b757e37f064", null ],
+    [ "SacnDmxMergerSource", "group__sacn__dmx__merger.html#ga2c69b926b913a0baa225106584012459", null ],
+    [ "sacn_dmx_merger_add_source", "group__sacn__dmx__merger.html#ga1c3ed232fc3f7c23c914587fa387cd6b", null ],
+    [ "sacn_dmx_merger_create", "group__sacn__dmx__merger.html#ga8758dad93531c1a5bbf4643157fe2c72", null ],
+    [ "sacn_dmx_merger_destroy", "group__sacn__dmx__merger.html#ga539b249ea5d0898efce4ee7371fc91ef", null ],
+    [ "sacn_dmx_merger_get_source", "group__sacn__dmx__merger.html#gab9f34e5fab687a2d4fdeef43ea3b2af4", null ],
+    [ "sacn_dmx_merger_remove_paps", "group__sacn__dmx__merger.html#gaa3057d877c09ef61ad68813c68b0294f", null ],
+    [ "sacn_dmx_merger_remove_source", "group__sacn__dmx__merger.html#gacd441710a3047265429cb1682c50a402", null ],
+    [ "sacn_dmx_merger_update_levels", "group__sacn__dmx__merger.html#gafcf58136c0c882d05724ff6bd57f78a8", null ],
+    [ "sacn_dmx_merger_update_paps", "group__sacn__dmx__merger.html#ga574eeb1eda51e31eb13fe12ccb543d42", null ],
+    [ "sacn_dmx_merger_update_universe_priority", "group__sacn__dmx__merger.html#gaf31312d80e0f9a198e6cb1e27ea4d63c", null ]
+];
---
+++ docs/stage/32/navtreedata.js
@@ -0,0 +1,47 @@
+var NAVTREE =
+[
+  [ "sACN", "index.html", [
+    [ "Streaming ACN", "index.html", null ],
+    [ "Additional Documentation", "additional_documentation.html", "additional_documentation" ],
+    [ "Getting Started with sACN", "getting_started.html", "getting_started" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"additional_documentation.html",
+"group__sacn__merge__receiver.html",
+"group__sacnopts__source__detector.html#gae3fd4d040877d49d52ab3b52e64897e5"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
