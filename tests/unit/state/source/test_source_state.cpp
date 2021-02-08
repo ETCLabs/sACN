e720fc95cbeec52498bacc9df6b2832813bf7cbb
ChristianReese <christian.g.reese96@gmail.com>
2021-02-08 12:42:37 -0600

    Added test_source_state to CMake

---
+++ tests/unit/state/source/test_source_state.cpp
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright 2020 ETC Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************
+ * This file is a part of sACN. For more information, go to:
+ * https://github.com/ETCLabs/sACN
+ *****************************************************************************/
+
+#include "sacn/private/source_state.h"
+
+#include <limits>
+#include "etcpal_mock/common.h"
+#include "sacn_mock/private/common.h"
+#include "sacn_mock/private/sockets.h"
+#include "sacn/private/mem.h"
+#include "sacn/private/opts.h"
+#include "gtest/gtest.h"
+#include "fff.h"
+
+#if SACN_DYNAMIC_MEM
+#define TestSourceState TestSourceStateDynamic
+#else
+#define TestSourceState TestSourceStateStatic
+#endif
+
+class TestSourceState : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    etcpal_reset_all_fakes();
+    sacn_common_reset_all_fakes();
+    sacn_sockets_reset_all_fakes();
+
+    ASSERT_EQ(sacn_mem_init(1), kEtcPalErrOk);
+    ASSERT_EQ(sacn_source_state_init(), kEtcPalErrOk);
+  }
+
+  void TearDown() override
+  {
+    sacn_source_state_deinit();
+    sacn_mem_deinit();
+  }
+};
+
+TEST_F(TestSourceState, Foo)
+{
+  // TODO
+}
