1c082b3cace8523daaff3a63ee7546b4b6054d2b
ETCLabs CI <noreply.etclabs@etcconnect.com>
2021-02-09 20:41:16 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20210209.1
    
    Commit: 8139aef4333c1d236c5c87d79e5039ec866d5418

---
+++ docs/stage/37/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['additional_20documentation',['Additional Documentation',['https://etclabs.github.io/EtcPal/docs/head/additional_docs.html',1,'']]],
+  ['additional_20documentation',['Additional Documentation',['../additional_documentation.html',1,'']]]
+];
---
+++ docs/stage/37/search/variables_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['err',['err',['https://etclabs.github.io/EtcPal/docs/head/struct_etc_pal_poll_event.html#a13b5fda068afcd9c79a54263a9fe7b07',1,'EtcPalPollEvent']]],
+  ['events',['events',['https://etclabs.github.io/EtcPal/docs/head/struct_etc_pal_poll_event.html#a8c1a86a9a616989de3fcced4b0c0e1f7',1,'EtcPalPollEvent']]]
+];
