ea1b7edbb12115419e2ccde694796e3b5ba5220d
ETCLabs CI <noreply.etclabs@etcconnect.com>
2021-02-26 21:44:32 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20210226.4
    
    Commit: 620b34100fa2e619ba6a39fc0ab33864c6c693b4

---
+++ docs/stage/49/group__s_a_c_n.js
@@ -0,0 +1,37 @@
+var group__s_a_c_n =
+[
+    [ "sACN DMX Merger", "group__sacn__dmx__merger.html", "group__sacn__dmx__merger" ],
+    [ "sACN Merge Receiver", "group__sacn__merge__receiver.html", "group__sacn__merge__receiver" ],
+    [ "sACN Receiver", "group__sacn__receiver.html", "group__sacn__receiver" ],
+    [ "sACN Source", "group__sacn__source.html", "group__sacn__source" ],
+    [ "sACN Source Detector", "group__sacn__source__detector.html", "group__sacn__source__detector" ],
+    [ "sACN Configuration Options", "group__sacnopts.html", "group__sacnopts" ],
+    [ "SacnHeaderData", "struct_sacn_header_data.html", [
+      [ "cid", "struct_sacn_header_data.html#a4c3f7d9a58af5033f9acd8c942bf81d9", null ],
+      [ "preview", "struct_sacn_header_data.html#a631d1206624dc91b6c574bf919a7699d", null ],
+      [ "priority", "struct_sacn_header_data.html#a0ad043071ccc7a261d79a759dc9c6f0c", null ],
+      [ "slot_count", "struct_sacn_header_data.html#a59d8c182097049f06609faab81891542", null ],
+      [ "source_name", "struct_sacn_header_data.html#a176bd2902909dd199b4c4d03c1cae8cd", null ],
+      [ "start_code", "struct_sacn_header_data.html#a09df16493b12c991aef309819c8eebab", null ],
+      [ "universe_id", "struct_sacn_header_data.html#a7e0de4d0f4e4bbd14771257febdf7248", null ]
+    ] ],
+    [ "SacnMcastInterface", "struct_sacn_mcast_interface.html", [
+      [ "iface", "struct_sacn_mcast_interface.html#a9d398e27f9eb3b1ee3b422225196b684", null ],
+      [ "status", "struct_sacn_mcast_interface.html#a3ed2ca570213574abb27e4d16cab480b", null ]
+    ] ],
+    [ "DMX_ADDRESS_COUNT", "group__s_a_c_n.html#ga681f92a30c76ae426e2403a328978abb", null ],
+    [ "SACN_SOURCE_NAME_MAX_LEN", "group__s_a_c_n.html#ga0a68bef69f737e31072b475521d331c8", null ],
+    [ "SACN_VERSION_BUILD", "group__s_a_c_n.html#gab22648c510945c218b806ad28e1e9a86", null ],
+    [ "SACN_VERSION_MAJOR", "group__s_a_c_n.html#ga3b04b863e88d1bae02133dfb19667e06", null ],
+    [ "SACN_VERSION_MINOR", "group__s_a_c_n.html#ga234bcbd2198002c6ee8d3caa670ba0ed", null ],
+    [ "SACN_VERSION_PATCH", "group__s_a_c_n.html#gae1d8849ebfa2d27cec433e54f7f7308d", null ],
+    [ "SacnHeaderData", "group__s_a_c_n.html#ga2e6652620c783917abe8e5c6b5d9d8b7", null ],
+    [ "SacnMcastInterface", "group__s_a_c_n.html#gab240ac59938a557843efa9068b74e1d4", null ],
+    [ "sacn_ip_support_t", "group__s_a_c_n.html#ga02f82b9c734e2d2f70a1106d6480833a", [
+      [ "kSacnIpV4Only", "group__s_a_c_n.html#gga02f82b9c734e2d2f70a1106d6480833aa900230541148a1eb50b457dfbf75a3c4", null ],
+      [ "kSacnIpV6Only", "group__s_a_c_n.html#gga02f82b9c734e2d2f70a1106d6480833aaa7c7cce56d48e45e33bde272cb0be424", null ],
+      [ "kSacnIpV4AndIpV6", "group__s_a_c_n.html#gga02f82b9c734e2d2f70a1106d6480833aa946f61a87ca52ca76b687484c5cdced2", null ]
+    ] ],
+    [ "sacn_deinit", "group__s_a_c_n.html#ga7b80ebcafe9eb3240a67785377872f9a", null ],
+    [ "sacn_init", "group__s_a_c_n.html#ga06e5eacb42e03b67c65ed299500a8366", null ]
+];
