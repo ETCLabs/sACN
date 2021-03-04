15707cded5d65f91d67466f2927cee6e5d9fb4ca
ETCLabs CI <noreply.etclabs@etcconnect.com>
2021-03-04 23:37:41 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20210304.2
    
    Commit: bdf80a425a4d182e816eb20665d98ec0babbfadb

---
+++ docs/stage/52/search/groups_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['opaque_5fid_20_28strongly_20typed_20handles_29',['opaque_id (Strongly typed handles)',['https://etclabs.github.io/EtcPal/docs/head/group__etcpal__cpp__opaque__id.html',1,'']]],
+  ['os_20abstraction_20modules',['OS Abstraction Modules',['https://etclabs.github.io/EtcPal/docs/head/group__etcpal__os.html',1,'']]]
+];
