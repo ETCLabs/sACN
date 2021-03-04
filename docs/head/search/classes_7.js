b63fdbc745dae29bf456bb6c4d839a84a41b8942
ETCLabs CI <noreply.etclabs@etcconnect.com>
2021-03-04 15:21:49 +0000

    Deploy code docs to GitHub Pages
    Azure build: 20210304.1
    
    Commit: 598f707f778babf97aa28cd206303503a97c2149

--- docs/head/search/classes_7.js
+++ docs/head/search/classes_7.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['netintlist',['NetintList',['../structsacn_1_1_receiver_1_1_netint_list.html',1,'sacn::Receiver']]],
-  ['notifyhandler',['NotifyHandler',['../classsacn_1_1_receiver_1_1_notify_handler.html',1,'Receiver::NotifyHandler'],['../classsacn_1_1_source_detector_1_1_notify_handler.html',1,'SourceDetector::NotifyHandler'],['../classsacn_1_1_merge_receiver_1_1_notify_handler.html',1,'MergeReceiver::NotifyHandler']]]
+  ['notifyhandler',['NotifyHandler',['../classsacn_1_1_merge_receiver_1_1_notify_handler.html',1,'MergeReceiver::NotifyHandler'],['../classsacn_1_1_receiver_1_1_notify_handler.html',1,'Receiver::NotifyHandler'],['../classsacn_1_1_source_detector_1_1_notify_handler.html',1,'SourceDetector::NotifyHandler']]]
 ];
