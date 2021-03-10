8071bd2ed18191f233efdb4269f6789da576b704
Christian Reese <Christian.Reese@etcconnect.com>
2021-03-10 17:12:02 -0600

    TerminatedSourceGoesOfflineCorrectly

--- tests/unit/state/receiver/test_receiver_state.cpp
+++ tests/unit/state/receiver/test_receiver_state.cpp
@@ -1277,3 +1277,32 @@ TEST_F(TestReceiverThread, TimedOutSourceGoesOfflineCorrectly)
   RunThreadCycle();
   EXPECT_EQ(mark_sources_offline_fake.call_count, 3u);
 }
+
+TEST_F(TestReceiverThread, TerminatedSourceGoesOfflineCorrectly)
+{
+  auto source_is_offline = [](const SacnLostSourceInternal* offline_sources, size_t num_offline_sources,
+                              const SacnRemoteSourceInternal*, size_t, TerminationSet**, uint32_t) {
+    EXPECT_EQ(num_offline_sources, 1u);
+    EXPECT_EQ(ETCPAL_UUID_CMP(&offline_sources->cid, &kTestCid), 0);
+  };
+  auto source_is_not_offline = [](const SacnLostSourceInternal*, size_t num_offline_sources,
+                                  const SacnRemoteSourceInternal*, size_t, TerminationSet**,
+                                  uint32_t) { EXPECT_EQ(num_offline_sources, 0u); };
+
+  // Online
+  mark_sources_offline_fake.custom_fake = source_is_not_offline;
+
+  InitTestData(0x00u, kTestUniverse, kTestBuffer.data(), kTestBuffer.size());
+  RunThreadCycle();
+  etcpal_getms_fake.return_val += (SACN_PERIODIC_INTERVAL + 1u);
+  RunThreadCycle();
+  EXPECT_EQ(mark_sources_offline_fake.call_count, 1u);
+
+  // Offline
+  mark_sources_offline_fake.custom_fake = source_is_offline;
+
+  InitTestData(0x00u, kTestUniverse, kTestBuffer.data(), kTestBuffer.size(), SACN_OPTVAL_TERMINATED);
+  etcpal_getms_fake.return_val += (SACN_PERIODIC_INTERVAL + 1u);
+  RunThreadCycle();
+  EXPECT_EQ(mark_sources_offline_fake.call_count, 2u);
+}
