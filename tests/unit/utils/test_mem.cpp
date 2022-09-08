3fc134efcab1ddfc1cefd723ae71fdad6d5f7985
Christian Reese <Christian.Reese@etcconnect.com>
2022-09-08 18:32:37 -0500

    Merge receiver unit test build fixes

--- tests/unit/utils/test_mem.cpp
+++ tests/unit/utils/test_mem.cpp
@@ -838,13 +838,13 @@ TEST_F(TestMem, AddSacnMergeReceiverSourceWorks)
     EXPECT_EQ(etcpal_rbtree_size(&merge_receiver->sources), i);
     last_cid = etcpal::Uuid::V4();
     source_info.handle = static_cast<sacn_remote_source_t>(i);
-    EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false), kEtcPalErrOk);
+    EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false, false), kEtcPalErrOk);
   }
 
   EXPECT_EQ(etcpal_rbtree_size(&merge_receiver->sources), kNumSources);
 
   source_info.handle = static_cast<sacn_remote_source_t>(kNumSources - 1u);
-  EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false), kEtcPalErrExists);
+  EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false, false), kEtcPalErrExists);
 
   EXPECT_EQ(etcpal_rbtree_size(&merge_receiver->sources), kNumSources);
 }
@@ -862,7 +862,7 @@ TEST_F(TestMem, RemoveSacnMergeReceiverSourceWorks)
   for (size_t i = 0u; i < kNumSources; ++i)
   {
     source_info.handle = static_cast<sacn_remote_source_t>(i);
-    EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false), kEtcPalErrOk);
+    EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false, false), kEtcPalErrOk);
   }
 
   for (size_t i = 0u; i < kNumSources; ++i)
@@ -918,7 +918,7 @@ TEST_F(TestMem, RespectsMaxMergeReceiverSourceLimit)
   for (int i = 0; i < SACN_RECEIVER_TOTAL_MAX_SOURCES; ++i)
   {
     source_info.handle = static_cast<sacn_remote_source_t>(i);
-    EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false), kEtcPalErrOk);
+    EXPECT_EQ(add_sacn_merge_receiver_source(merge_receiver, &source_addr, &source_info, false, false), kEtcPalErrOk);
   }
 }
 
