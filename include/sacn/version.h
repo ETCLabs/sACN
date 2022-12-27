141efa118b01a1974b304e87ea4abdf98eac7b16
ETC CI <noreply.ci@etcconnect.com>
2022-12-27 17:25:41 +0000

    [skip ci] Update version files for build 3.0.0.4

--- include/sacn/version.h
+++ include/sacn/version.h
@@ -42,7 +42,7 @@
 #define SACN_VERSION_MAJOR 3 /**< The major version. */
 #define SACN_VERSION_MINOR 0 /**< The minor version. */
 #define SACN_VERSION_PATCH 0 /**< The patch version. */
-#define SACN_VERSION_BUILD 3 /**< The build number. */
+#define SACN_VERSION_BUILD 4 /**< The build number. */
 /**
  * @}
  */
@@ -51,8 +51,8 @@
  * @name sACN version strings
  * @{
  */
-#define SACN_VERSION_STRING      "3.0.0.3" /**< The 4-digit version string. */
-#define SACN_VERSION_DATESTR     "30.Nov.2022" /**< The date this version was released (dd.Mm.yyyy). */
+#define SACN_VERSION_STRING      "3.0.0.4" /**< The 4-digit version string. */
+#define SACN_VERSION_DATESTR     "27.Dec.2022" /**< The date this version was released (dd.Mm.yyyy). */
 #define SACN_VERSION_COPYRIGHT   "Copyright 2022 ETC Inc." /**< The version's copyright string. */
 #define SACN_VERSION_PRODUCTNAME "sACN" /**< The version's product name. */
 /**
