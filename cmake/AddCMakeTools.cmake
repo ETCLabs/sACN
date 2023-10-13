ff84b13e660aacf72146d889e1bd0a341ec26cb3
Christian Reese <Christian.Reese@etcconnect.com>
2023-10-13 13:51:15 -0500

    New EtcPal needs new CMake tools

--- cmake/AddCMakeTools.cmake
+++ cmake/AddCMakeTools.cmake
@@ -1,7 +1,7 @@
 # Add CMake Tools
 include(FetchContent)
 
-set(CMAKE_TOOLS_GIT_TAG d34e598c3e54f7ed89ab8cd6c281ffeb5a6e635a)
+set(CMAKE_TOOLS_GIT_TAG de21bc230599ed27347e18946fe56dcec33a097d)
 if(DEFINED ENV{CI})
   if(DEFINED ENV{JENKINS_CMAKE_TOOLS_TOKEN})
     set(CMAKE_TOOLS_GIT_REPO https://$ENV{JENKINS_CMAKE_TOOLS_TOKEN}@gitlab.etcconnect.com/etc/common-tech/tools/cmake-tools.git)
