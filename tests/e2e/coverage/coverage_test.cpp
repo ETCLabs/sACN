329d33bd59a54d3380b46a1b142e806b58951517
Christian Reese <Christian.Reese@etcconnect.com>
2024-02-02 17:08:36 -0600

    E2E pt.2

--- tests/e2e/coverage/coverage_test.cpp
+++ tests/e2e/coverage/coverage_test.cpp
@@ -32,9 +32,14 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#include <array>
+#include <cstdlib>
+#include <ctime>
+#include <unordered_map>
 #include <unordered_set>
 
 using testing::_;
+using testing::AtLeast;
 using testing::InSequence;
 using testing::Invoke;
 using testing::NiceMock;
@@ -45,6 +50,10 @@ using testing::Return;
 #else
 #define CoverageTest CoverageTestStatic
 #endif
+
+using UniverseId = uint16_t;
+
+static constexpr UniverseId kDefaultUniverse = 1u;
 
 class MockLogMessageHandler : public etcpal::LogMessageHandler
 {
@@ -89,44 +98,190 @@ public:
   MOCK_METHOD(void, HandleMemoryLimitExceeded, (), (override));
 };
 
-class TestSource
+class TestMergeReceiver
 {
 public:
-  struct ValueRange
+  TestMergeReceiver() = default;
+
+  ~TestMergeReceiver()
   {
-    ValueRange() = default;
-    ValueRange(int val) : min(val), max(val) {}
+    for (auto& [universe_id, state] : universes_)
+      state.merge_receiver.Shutdown();
+  }
 
-    int min{0};
-    int max{0};
-  };
-
-  struct StartCodeParams
+  void AddUniverse(UniverseId universe_id = kDefaultUniverse)
   {
-    uint8_t code{0u};
-    ValueRange value;
-  };
+    ASSERT_EQ(universes_.find(universe_id), universes_.end());
 
-  struct UniverseParams
+    UniverseState& state = universes_[universe_id];
+    EXPECT_TRUE(state.merge_receiver.Startup(sacn::MergeReceiver::Settings(universe_id), state.notify));
+  }
+
+  MockMergeReceiverNotifyHandler& GetNotifyHandlerForUniverse(UniverseId universe_id = kDefaultUniverse)
   {
-    std::vector<StartCodeParams> start_codes;
-    int updates_per_second{300};
-  };
-
-  TestSource() = default;
-
-  void AddUniverse(const UniverseParams& universe_params);
+    EXPECT_NE(universes_.find(universe_id), universes_.end());
+    return universes_[universe_id].notify;
+  }
 
 private:
   struct UniverseState
   {
-    UniverseParams params;
+    sacn::MergeReceiver merge_receiver;
+    NiceMock<MockMergeReceiverNotifyHandler> notify;
+  };
+
+  std::unordered_map<UniverseId, UniverseState> universes_;
+};
+
+class TestSourceDetector
+{
+public:
+  TestSourceDetector() = default;
+  ~TestSourceDetector() { sacn::SourceDetector::Shutdown(); }
+
+  void Startup() { EXPECT_TRUE(sacn::SourceDetector::Startup(notify_)); }
+  MockSourceDetectorNotifyHandler& GetNotifyHandler() { return notify_; }
+
+private:
+  NiceMock<MockSourceDetectorNotifyHandler> notify_;
+};
+
+class TestSource
+{
+public:
+  struct StartCodeParams
+  {
+    int code;
+    int value;
+    std::optional<int> min;
+    std::optional<int> max;
+  };
+
+  struct UniverseParams
+  {
+    uint16_t universe{kDefaultUniverse};
+    uint8_t universe_priority{100u};
+    std::vector<StartCodeParams> start_codes;
+  };
+
+  TestSource()
+  {
+    auto cid = etcpal::Uuid::V4();
+    EXPECT_TRUE(source_.Startup(sacn::Source::Settings(cid, std::string("Test Source ") + cid.ToString())));
+  }
+
+  ~TestSource()
+  {
+    for (auto& [universe_id, state] : universes_)
+      state.terminate.Notify();
+
+    for (auto& [universe_id, state] : universes_)
+      state.thread.Join();
+
+    source_.Shutdown();
+  }
+
+  void AddUniverse(const UniverseParams& params)
+  {
+    ASSERT_EQ(universes_.find(params.universe), universes_.end());
+
+    UniverseState& state = universes_[params.universe];
+
+    for (const auto& start_code : params.start_codes)
+    {
+      switch (start_code.code)
+      {
+        case SACN_STARTCODE_DMX:
+          state.null_start_code = StartCodeState(start_code);
+          break;
+        case SACN_STARTCODE_PRIORITY:
+          state.pap_start_code = StartCodeState(start_code);
+          break;
+        default:
+          state.custom_start_codes.emplace_back(start_code);
+      }
+    }
+
+    sacn::Source::UniverseSettings settings;
+    settings.universe = params.universe;
+    settings.priority = params.universe_priority;
+    EXPECT_TRUE(source_.AddUniverse(settings));
+
+    state.thread.Start([this, params, &state]() {
+      while (!state.terminate.TryWait())
+      {
+        UniverseTick(params.universe, state);
+        etcpal::Thread::Sleep(kUniverseSleepMs);
+      }
+    });
+  }
+
+  void RemoveUniverse(UniverseId universe_id = kDefaultUniverse)
+  {
+    ASSERT_NE(universes_.find(universe_id), universes_.end());
+
+    universes_[universe_id].terminate.Notify();
+    universes_[universe_id].thread.Join();
+
+    universes_.erase(universe_id);
+
+    source_.RemoveUniverse(universe_id);
+  }
+
+private:
+  static constexpr unsigned int kUniverseSleepMs = 100u;
+
+  struct StartCodeState
+  {
+    StartCodeState() = default;
+    StartCodeState(const StartCodeParams& p) : params(p) { buffer.fill(static_cast<uint8_t>(p.value)); }
+
+    StartCodeParams params;
+    std::array<uint8_t, DMX_ADDRESS_COUNT> buffer{};
+  };
+
+  struct UniverseState
+  {
+    StartCodeState null_start_code;
+    std::optional<StartCodeState> pap_start_code;
+    std::vector<StartCodeState> custom_start_codes;
     etcpal::Thread thread;
     etcpal::Signal terminate;
   };
 
+  void UniverseTick(UniverseId universe_id, UniverseState& state)
+  {
+    UpdateStartCodeData(state.null_start_code);
+    if (state.pap_start_code)
+    {
+      UpdateStartCodeData(*state.pap_start_code);
+      source_.UpdateLevelsAndPap(universe_id, state.null_start_code.buffer.data(), state.null_start_code.buffer.size(),
+                                 state.pap_start_code->buffer.data(), state.pap_start_code->buffer.size());
+    }
+    else
+    {
+      source_.UpdateLevels(universe_id, state.null_start_code.buffer.data(), state.null_start_code.buffer.size());
+    }
+
+    for (auto& custom_code : state.custom_start_codes)
+    {
+      UpdateStartCodeData(custom_code);
+      source_.SendNow(universe_id, static_cast<uint8_t>(custom_code.params.code), custom_code.buffer.data(),
+                      custom_code.buffer.size());
+    }
+  }
+
+  void UpdateStartCodeData(StartCodeState& state)
+  {
+    if (state.params.min && state.params.max)
+    {
+      for (uint8_t& slot : state.buffer)
+        slot = static_cast<uint8_t>(rand() % (*state.params.max - *state.params.min + 1) + *state.params.min);
+    }
+  }
+
   sacn::Source source_;
-  std::vector<UniverseState> universes;
+  std::unordered_map<UniverseId, UniverseState> universes_;
 };
 
 class CoverageTest : public ::testing::Test
@@ -134,6 +289,8 @@ class CoverageTest : public ::testing::Test
 protected:
   void SetUp() override
   {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
     EXPECT_TRUE(logger_.Startup(mock_log_handler_));
     EXPECT_TRUE(sacn::Init(logger_));
   }
@@ -146,12 +303,87 @@ protected:
 
   etcpal::Logger logger_;
 
-  std::vector<TestSource> sources_;
-
   NiceMock<MockLogMessageHandler> mock_log_handler_;
 };
 
-TEST_F(CoverageTest, SourceToMergeReceiverAndSourceDetector)
+TEST_F(CoverageTest, SendAndReceiveSimpleUniverse)
 {
-  // TODO
+  TestMergeReceiver merge_receiver;
+  merge_receiver.AddUniverse();
+
+  EXPECT_CALL(merge_receiver.GetNotifyHandlerForUniverse(), HandleMergedData(_, _)).Times(AtLeast(1));
+  EXPECT_CALL(merge_receiver.GetNotifyHandlerForUniverse(), HandleSamplingPeriodStarted(_, _)).Times(AtLeast(1));
+  EXPECT_CALL(merge_receiver.GetNotifyHandlerForUniverse(), HandleSamplingPeriodEnded(_, _)).Times(AtLeast(1));
+
+  TestSource source;
+  source.AddUniverse({.start_codes = {{.code = SACN_STARTCODE_DMX, .value = 0xFF}}});
+
+  etcpal::Thread::Sleep(2000u);  // Cover sampling period
 }
+
+TEST_F(CoverageTest, SendReceiveAndMergeAtScale)
+{
+  static constexpr UniverseId kTestUniverses[] = {1u, 2u, 3u, 4u, 5u, 6u, 7u};
+  static constexpr int kNumTestSources = 7u;
+
+  TestMergeReceiver merge_receiver;
+  for (UniverseId universe_id : kTestUniverses)
+  {
+    merge_receiver.AddUniverse(universe_id);
+    EXPECT_CALL(merge_receiver.GetNotifyHandlerForUniverse(universe_id), HandleMergedData(_, _)).Times(AtLeast(1));
+    EXPECT_CALL(merge_receiver.GetNotifyHandlerForUniverse(universe_id), HandleSamplingPeriodStarted(_, _))
+        .Times(AtLeast(1));
+    EXPECT_CALL(merge_receiver.GetNotifyHandlerForUniverse(universe_id), HandleSamplingPeriodEnded(_, _))
+        .Times(AtLeast(1));
+  }
+
+  std::vector<std::unique_ptr<TestSource>> sources;
+  for (int i = 0; i < kNumTestSources; ++i)
+  {
+    auto source = std::make_unique<TestSource>();
+    ASSERT_TRUE(source);
+    for (UniverseId universe_id : kTestUniverses)
+    {
+      source->AddUniverse({.universe = universe_id,
+                           .start_codes = {{.code = SACN_STARTCODE_DMX, .min = 0x00, .max = 0xFF},
+                                           {.code = SACN_STARTCODE_PRIORITY, .min = 0x00, .max = 0xFF}}});
+    }
+
+    sources.push_back(std::move(source));
+  }
+
+  etcpal::Thread::Sleep(2000u);  // Cover sampling period
+}
+
+#if 0  // TODO: WIP - fix sacn_config.h issue (sACN is being built and linked separately)
+
+TEST_F(CoverageTest, DetectSourcesComingAndGoing)
+{
+  static constexpr UniverseId kTestUniverses[] = {1u, 2u, 3u, 4u, 5u, 6u, 7u};
+  static constexpr int kNumTestSources = 7u;
+
+  TestSourceDetector source_detector;
+  source_detector.Startup();
+  EXPECT_CALL(source_detector.GetNotifyHandler(), HandleSourceUpdated(_, _, _, _)).Times(AtLeast(1));
+  EXPECT_CALL(source_detector.GetNotifyHandler(), HandleSourceExpired(_, _, _)).Times(AtLeast(1));
+
+  std::vector<std::unique_ptr<TestSource>> sources;
+  for (int i = 0; i < kNumTestSources; ++i)
+  {
+    auto source = std::make_unique<TestSource>();
+    ASSERT_TRUE(source);
+    for (UniverseId universe_id : kTestUniverses)
+      source->AddUniverse({.universe = universe_id, .start_codes = {{.code = SACN_STARTCODE_DMX, .value = 0xFF}}});
+
+    sources.push_back(std::move(source));
+  }
+
+  etcpal::Thread::Sleep(500u);  // Some time to detect sources
+
+  for (int i = 0; i < kNumTestSources; ++i)
+    sources.pop_back();
+
+  etcpal::Thread::Sleep(21000u);  // Cover universe discovery expiration
+}
+
+#endif
