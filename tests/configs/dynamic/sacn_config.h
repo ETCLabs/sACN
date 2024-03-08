d03ca9ec6a34c284922ab90266f36d18f3dee63d
Christian Reese <Christian.Reese@etcconnect.com>
2024-03-08 15:21:03 -0600

    #define SACN_RECEIVER_MAX_SUBS_PER_SOCKET 10

--- tests/configs/dynamic/sacn_config.h
+++ tests/configs/dynamic/sacn_config.h
@@ -11,3 +11,4 @@ bool SacnTestingAssertHandler(const char* expression, const char* file, const ch
 #define SACN_ASSERT_VERIFY(expr) ((expr) ? true : SacnTestingAssertHandler(#expr, __FILE__, __func__, __LINE__))
 #define SACN_LOGGING_ENABLED 0
 #define SACN_DYNAMIC_MEM 1
+#define SACN_RECEIVER_MAX_SUBS_PER_SOCKET 10
