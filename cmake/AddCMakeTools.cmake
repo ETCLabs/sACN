da39ac85ee02fa1ddeb1841bb1cc9fc680b5bb1f
Christian Reese <Christian.Reese@etcconnect.com>
2024-03-22 16:01:47 -0500

    Add -nostdinc++ flag

--- cmake/AddCMakeTools.cmake
+++ cmake/AddCMakeTools.cmake
@@ -1,7 +1,7 @@
 # Add CMake Tools
 include(FetchContent)
 
-set(CMAKE_TOOLS_GIT_TAG 98d05b8ce8e9b7d697e9804134e21c4bc501f9af)
+set(CMAKE_TOOLS_GIT_TAG 281d156508fefac98ca59cdec9067c89ae3d6d10)
 if(DEFINED ENV{CI})
   if(DEFINED ENV{JENKINS_CMAKE_TOOLS_TOKEN})
     set(CMAKE_TOOLS_GIT_REPO https://$ENV{JENKINS_CMAKE_TOOLS_TOKEN}@gitlab.etcconnect.com/etc/common-tech/tools/cmake-tools.git)
