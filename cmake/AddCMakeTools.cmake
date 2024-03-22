0ffe7cfc63b0356392733668878248babfe87e6b
Christian Reese <Christian.Reese@etcconnect.com>
2024-03-22 15:20:26 -0500

    Maybe remove it from the compile flags too?

--- cmake/AddCMakeTools.cmake
+++ cmake/AddCMakeTools.cmake
@@ -1,7 +1,7 @@
 # Add CMake Tools
 include(FetchContent)
 
-set(CMAKE_TOOLS_GIT_TAG 169991c37f69f97adfbbfcaf0a7d6cf2a2b903db)
+set(CMAKE_TOOLS_GIT_TAG 0d53cbe82cc3302ec6362650ef9fbb22fa54b573)
 if(DEFINED ENV{CI})
   if(DEFINED ENV{JENKINS_CMAKE_TOOLS_TOKEN})
     set(CMAKE_TOOLS_GIT_REPO https://$ENV{JENKINS_CMAKE_TOOLS_TOKEN}@gitlab.etcconnect.com/etc/common-tech/tools/cmake-tools.git)
